086ffe6ad724f1456d0bf0b0f0a9cd04c5c54804
Carlos Zubiran <carloszubiran@gmail.com>
2016-05-18 15:51:26 -0500

    Initial commit

---
+++ src/MySQLTableCreation.java
@@ -0,0 +1,27 @@
+/**
+ * Created by Carlos Zubiran on 5/18/2016.
+ */
+public class MySQLTableCreation {
+
+    //region PROPERTIES
+
+
+    //endregion PROPERTIES
+
+    //region CONSTRUCTORS
+
+
+    //endregion CONSTRUCTORS
+
+    //region GETTERS / SETTERS
+
+
+    //endregion GETTERS / SETTERS
+
+    //region CUSTOM METHODS
+
+
+    //endregion CUSTOM METHODS
+
+
+}
